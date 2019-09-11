605f4ff920c17275ecb1fcc38a1617e4cf5ed704
julia <juliajaca@hotmail.com>
2019-09-11 11:48:38 +0200

    Inicio del proyecto

---
+++ blog/blog.py
@@ -0,0 +1,16 @@
+'''
+Blog de notas de Julia
+---------------------
+v1.0
+=======================
+Ejercicio que puntúa hasta 3 puntos para el examen final del módulo
+Sin internet
+Sólo evernote
+No hay slack
+Para terminal
+Pensar bien las  opciones que tendra el blog de notas
+Diagramacion wwww.lucidchart.com (diagrama de flujo)
+En el entorno virtual de 3.7.4
+git local
+al finalizar el ejercicio a las 16:15 enviar enlace por github
+'''
